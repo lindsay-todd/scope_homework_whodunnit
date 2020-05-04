1326d8b5b0f85c7000bf8a53d0d313fe167ccce7
lindsay-todd <firecrackerrecordings@gmail.com>
2020-05-04 22:32:22 +0100

    homework answers done

---
+++ scope_homework_whodunnit.js
@@ -0,0 +1,209 @@
+// Brief
+
+// Using your knowledge about scope and variable declarations in JavaScript,
+// look at the following code snippets and predict what the output or error will
+// be and why. Copy the following episodes into a JavaScript file and add comments
+// under each one detailing the reason for your predicted output.
+
+// MVP
+
+// Episode 1
+
+// const scenario = {
+//   murderer: 'Miss Scarlet',
+//   room: 'Library',
+//   weapon: 'Rope'
+// };
+
+// const declareMurderer = function() {
+//   return `The murderer is ${scenario.murderer}.`;
+// }
+
+// const verdict = declareMurderer();
+// console.log(verdict);
+
+// The predicted output would be 'The murderer is Miss Scarlet' because we are calling the murderer key from scenario.
+
+// Episode 2
+
+// const murderer = 'Professor Plum';
+
+// const changeMurderer = function() {
+//   murderer = 'Mrs. Peacock';
+// }
+
+// const declareMurderer = function() {
+//   return `The murderer is ${murderer}.`;
+// }
+
+// changeMurderer();
+// const verdict = declareMurderer();
+// console.log(verdict);
+
+// Think it would throw an error as we're trying to change a variable with a constant outside the scope.
+
+// // Episode 3
+
+// let murderer = 'Professor Plum';
+
+// const declareMurderer = function() {
+//   let murderer = 'Mrs. Peacock';
+//   return `The murderer is ${murderer}.`;
+// }
+
+// const firstVerdict = declareMurderer();
+// console.log('First Verdict: ', firstVerdict);
+
+// const secondVerdict = `The murderer is ${murderer}.`;
+// console.log('Second Verdict: ', secondVerdict);
+
+// First Verdict: The murderer is Mrs. Peacock because the function declareMurderer is called from inside the const. Second Verdict: The murderer is Professor Plum as it interpolates the let declaration with Professor Plum.
+
+
+// // Episode 4
+
+// let suspectOne = 'Miss Scarlet';
+// let suspectTwo = 'Professor Plum';
+// let suspectThree = 'Mrs. Peacock';
+
+// const declareAllSuspects = function() {
+//   let suspectThree = 'Colonel Mustard';
+//   return `The suspects are ${suspectOne}, ${suspectTwo}, ${suspectThree}.`;
+// }
+
+// const suspects = declareAllSuspects();
+// console.log(suspects);
+// console.log(`Suspect three is ${suspectThree}.`);
+
+//  Line 75 prints the variable suspects which runs the funtion to return 'The suspects are Miss Scarlet, Professor Plum, Colonel Mustard.' Line 76 prints Mrs. Peacock by accessing variable Suspect Three from line 67.
+
+// // Episode 5
+
+// const scenario = {
+//   murderer: 'Miss Scarlet',
+//   room: 'Kitchen',
+//   weapon: 'Candle Stick'
+// };
+
+// const changeWeapon = function(newWeapon) {
+//   scenario.weapon = newWeapon;
+// }
+
+// const declareWeapon = function() {
+//   return `The weapon is the ${scenario.weapon}.`;
+// }
+
+// changeWeapon('Revolver');
+// const verdict = declareWeapon();
+// console.log(verdict);
+
+// The weapon is the Revolver. Because the console will print verdict, which is the string with interpolation and that will return 'The weapon is the Candlestick'. But the key value pair is updated with the ChangeWeapon variable which holds a newWeapon function, returning The Revolver.
+
+// // Episode 6
+
+// let murderer = 'Colonel Mustard';
+
+// const changeMurderer = function() {
+//   murderer = 'Mr. Green';
+
+//   const plotTwist = function() {
+//     murderer = 'Mrs. White';
+//   }
+
+//   plotTwist();
+// }
+
+// const declareMurderer = function () {
+//   return `The murderer is ${murderer}.`;
+// }
+
+// changeMurderer();
+// const verdict = declareMurderer();
+// console.log(verdict);
+
+// The murderer is Mrs White. Because the verdict is printed to the console, calling the function declareMuderer which would be Colonel Mustard, this in turn gets superseeded by changeMurderer function which in turn calls the plotTwist function resulting in Mrs White!
+
+// // Episode 7
+
+// let murderer = 'Professor Plum';
+
+// const changeMurderer = function() {
+//   murderer = 'Mr. Green';
+
+//   const plotTwist = function() {
+//     let murderer = 'Colonel Mustard';
+
+//     const unexpectedOutcome = function() {
+//       murderer = 'Miss Scarlet';
+//     }
+
+//     unexpectedOutcome();
+//   }
+
+//   plotTwist();
+// }
+
+// const declareMurderer = function() {
+//   return `The murderer is ${murderer}.`;
+// }
+
+// changeMurderer();
+// const verdict = declareMurderer();
+// console.log(verdict);
+
+// This one had me stumped. I thought the answer would be Colonel Mustard but after pondering over the result I concluded that the const changeMurderer takes precedence over the order of Constants.
+
+// // Episode 8
+
+// const scenario = {
+//   murderer: 'Mrs. Peacock',
+//   room: 'Conservatory',
+//   weapon: 'Lead Pipe'
+// };
+
+// const changeScenario = function() {
+//   scenario.murderer = 'Mrs. Peacock';
+//   scenario.room = 'Dining Room';
+
+//   const plotTwist = function(room) {
+//     if (scenario.room === room) {
+//       scenario.murderer = 'Colonel Mustard';
+//     }
+
+//     const unexpectedOutcome = function(murderer) {
+//       if (scenario.murderer === murderer) {
+//         scenario.weapon = 'Candle Stick';
+//       }
+//     }
+
+//     unexpectedOutcome('Colonel Mustard');
+//   }
+
+//   plotTwist('Dining Room');
+// }
+
+// const declareWeapon = function() {
+//   return `The weapon is ${scenario.weapon}.`
+// }
+
+// changeScenario();
+// const verdict = declareWeapon();
+// console.log(verdict);
+
+// I have zero scoobies about this one.
+// // Episode 9
+
+// let murderer = 'Professor Plum';
+
+// if (murderer === 'Professor Plum') {
+//   let murderer = 'Mrs. Peacock';
+// }
+
+// const declareMurderer = function() {
+//   return `The murderer is ${murderer}.`;
+// }
+
+// const verdict = declareMurderer();
+// console.log(verdict);
+
+// I have zero scoobies about this one as I thought it was Mrs Peacock.
